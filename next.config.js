e0c4b001c3820ac5775a5ee1b92a36a9a2dcbbf9
Sougata-LM <roy36tinku.1979@gmail.com>
2026-03-17 02:54:19 +0530

    fix: add basePath for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   output: 'export',
   trailingSlash: true,
   images: { unoptimized: true },
+  basePath:    isProd ? '/itzfizz-hero' : '',
+  assetPrefix: isProd ? '/itzfizz-hero' : '',
 }
